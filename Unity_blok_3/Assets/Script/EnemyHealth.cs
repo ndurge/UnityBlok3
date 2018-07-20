730e075ba3e6599eb1fe83b75b029e06c10266a9
ndurge <ninadurge@gmail.com>
2018-07-20 15:28:34 +0200

    Player en schieten opnieuw gedaan dmv Unity tutorial
    Had eerst de code van het blog gebruikt, maar ik kwam er niet echt uit, dus ik begon opnieuw met een tutorial.

---
+++ Unity_blok_3/Assets/Script/EnemyHealth.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHealth : MonoBehaviour //, IDamageable
+{
+
+    public int startingHealth = 3;
+    public GameObject hitParticles;
+
+    private int currentHealth;
+
+    void Start()
+    {
+        currentHealth = startingHealth;
+    }
+
+    public void Damage(int damage, Vector3 hitPoint)
+    {
+        Instantiate(hitParticles, hitPoint, Quaternion.identity);
+        currentHealth -= damage;
+        if (currentHealth <= 0)
+        {
+            Defeated();
+        }
+    }
+
+    void Defeated()
+    {
+        gameObject.SetActive(false);
+    }
+
+}
